40b0124c5826335f65e9c8a263c5d47e272619fd
Patrick B <pdbarnes@csu.fullerton.edu>
2018-10-09 11:13:47 -0700

    initial

---
+++ Lottery/Lottery.cs
@@ -0,0 +1,74 @@
+﻿/*
+    Patrick Barnes
+    https://github.com/pdbarnesCSUF/isds309_hw3
+*/
+/*
+-----
+  9
+-----
+Create a lottery game application named Lottery. Generate three 
+random numbers, each between 1 and 4. Allow the user to guess three
+ numbers. Compare each of the user’s guesses to the three random 
+ numbers, and display a message that includes the user’s guess, the
+ randomly determined three-digit number, and the amount of money the
+ user has won as follows:
+
+Matching Numbers	Award ($)
+Any one matching	10
+Two matching	100
+Three matching, not in order	1000
+Three matching in exact order	10,000
+No matches	0
+
+Make certain that your application accommodates repeating digits. 
+For example, if a user guesses 1, 2, and 3, and the randomly 
+generated digits are 1, 1, and 1, do not give the user credit 
+for three correct guesses—just one.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace Lottery
+{
+    class Lottery
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("hw3#9 - Patrick Barnes");
+            WriteLine("Lottery");
+
+            const int MIN = 1; //incl
+            const int MAX = 5; //excl
+            int first = 0, second = 0, third = 0;
+            int in_1 = 0, in_2 = 0, in_3 = 0;
+
+            Random ranNumberGenerator = new Random();
+            first  = ranNumberGenerator.Next(MIN, MAX);
+            second = ranNumberGenerator.Next(MIN, MAX);
+            third  = ranNumberGenerator.Next(MIN, MAX);
+
+            WriteLine("DEBUG: {0}, {1}, {2}", first, second, third); //DEBUG
+            WriteLine("What are your three numbers? (enter after each");
+            in_1 = int.Parse(ReadLine());
+            in_2 = int.Parse(ReadLine());
+            in_3 = int.Parse(ReadLine());
+
+            if (in_1 == first &&
+                in_2 == second &&
+                in_3 == third)
+            {
+                WriteLine("You've won: 10,000");
+            }
+            else
+            {
+
+            }
+
+            WriteLine("--End--");
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+**Assignment 3 - Chapter 4 - B**
+
+Due: Tuesday, Oct  9, 4 pm (before class)
+
+  
+
+#### **\*\*\* Please complete the following questions **
+
+Chapter 4, end of chapter review questions (p 17 - ).: 
+
+#### **6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20**
+
+##### **Attention**
+
+Write all your answers in **one (1)** word or text document. Save it as "**Ch4\_ReviewQuestions\_1.docx**" or "**Ch4\_ReviewQuestions\_1.txt**"  before uploading it to Titanium.
+
+  
+
+#### **\*\*\* Please complete the following  exercises.**
+
+Chapter 4, end of chapter exercises: 
+
+#### **8, 9** 
+
+**Attention**  
+
+Only upload the file "**program.cs**" for each exercise to Titanium. That is the file that contains the code you have written. before uploading it to Titanium, rename the file program.cs to FILENAME.txt and then upload the file. (FILENAME is the name that the each exercise has provided for each program)
+
+For example after completing chapter 2 exercise 6 (**InchesToCentimeterActive**), 
+
+*   Copy the file "**program.cs**" to a new location (that way you don't lose the original program.cs file). 
+*   Rename the newly copied file "**program.cs**" to "**InchesToCentimeterActive.txt**" 
+*   Upload the file "**InchesToCentimeterActive.txt**" to Titanium. 
+*   Repeat these steps for the other exercises.
+
+Good luck,,
